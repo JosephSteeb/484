dcf5972ac7b05510d38ec698abade5b7630f87f6
Tay Ho <tay.ho@wsu.edu>
2023-12-07 16:22:29 -0800

    Added a popup window to show alert message when alert is pushed.

--- theia/src/Monitor.js
+++ theia/src/Monitor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -13,24 +13,43 @@ const locationIcon = new L.Icon({
 
 const Monitor = () => {
 // Coordinates for Voiland College of Engineering and Architecture
-const position = [46.7304, -117.1752];
+const position = [46.7304, -117.165];
 
-return (
-  <div>
-  <MapContainer center={position} zoom={17} style={{ height: '500px', width: '100%' }}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    />
-    <Marker position={position} icon={locationIcon}>
-      <Popup>
-        Voiland College of Engineering and Architecture, Pullman, WA
-      </Popup>
-    </Marker>
-  </MapContainer>
-  <button onClick={() => console.log('clicked')}>Alert</button>
-  </div>
-);
+const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+
+  const handleAlertButtonClick = () => {
+    setModalMessage('Emergency Alerted!');
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <MapContainer center={position} zoom={17} style={{ height: '500px', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position} icon={locationIcon}>
+          <Popup>
+            Voiland College of Engineering and Architecture, Pullman, WA
+          </Popup>
+        </Marker>
+      </MapContainer>
+      <button onClick={handleAlertButtonClick}>Alert</button>
+
+      {showModal && (
+        <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', width: '80%', height: '70%', zIndex: 1000 }}>
+          <p>{modalMessage}</p>
+          <button onClick={handleCloseModal}>Close</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Monitor;
