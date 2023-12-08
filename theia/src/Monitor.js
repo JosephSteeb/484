1cf856d396286983db91924c462ceae7dffc3a94
Tay Ho <tay.ho@wsu.edu>
2023-12-07 16:00:12 -0800

    Embeded Google map.

--- theia/src/Monitor.js
+++ theia/src/Monitor.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import locationPointerIcon from './assets/location-pointer.svg';
+
+const locationIcon = new L.Icon({
+  iconUrl: locationPointerIcon,
+  iconRetinaUrl: locationPointerIcon,
+  iconSize: new L.Point(30, 30),
+  className: 'leaflet-div-icon'
+});
 
 const Monitor = () => {
-  return <h1>Hello, World!</h1>;
+// Coordinates for Voiland College of Engineering and Architecture
+const position = [46.7304, -117.1752];
+
+return (
+  <MapContainer center={position} zoom={17} style={{ height: '500px', width: '100%' }}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <Marker position={position} icon={locationIcon}>
+      <Popup>
+        Voiland College of Engineering and Architecture, Pullman, WA
+      </Popup>
+    </Marker>
+  </MapContainer>
+);
 };
 
 export default Monitor;
